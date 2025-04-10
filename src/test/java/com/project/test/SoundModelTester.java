a64207c8af4a5a09c2b818f3fce04aa1d7177e9e
rbaudu <3866570+rbaudu@users.noreply.github.com>
2025-04-10 07:49:01 +0200

    Mise à jour de SoundModelTester en façade pour les testeurs spécialisés

--- src/test/java/com/project/test/SoundModelTester.java
+++ src/test/java/com/project/test/SoundModelTester.java
@@ -1,50 +1,117 @@
 package com.project.test;
 
-import org.nd4j.linalg.dataset.api.iterator.DataSetIterator;
+import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.IOException;
 import java.util.Properties;
 
 /**
- * Testeur pour le modèle de détection de sons
+ * Testeur pour les modèles de détection de sons
+ * Sert de façade pour les différents types de testeurs spécialisés selon l'approche utilisée
  */
-public class SoundModelTester extends BaseModelTester implements ModelTester {
+public class SoundModelTester implements ModelTester {
     private static final Logger log = LoggerFactory.getLogger(SoundModelTester.class);
     
+    // Types de testeurs de sons
+    public enum SoundModelType {
+        MFCC,
+        SPECTROGRAM
+    }
+    
+    private ModelTester tester;
+    private SoundModelType modelType;
+    private Properties config;
+    
+    /**
+     * Constructeur avec configuration
+     * 
+     * @param config Propriétés de configuration
+     */
     public SoundModelTester(Properties config) {
-        super(config, "sound");
+        this.config = config;
+        
+        // Déterminer le type de testeur
+        String modelTypeStr = config.getProperty("sound.model.type", "STANDARD");
+        if ("SPECTROGRAM".equalsIgnoreCase(modelTypeStr)) {
+            this.modelType = SoundModelType.SPECTROGRAM;
+        } else {
+            this.modelType = SoundModelType.MFCC;
+        }
+        
+        // Créer le testeur approprié
+        createTester();
+    }
+    
+    /**
+     * Constructeur avec type de testeur spécifié
+     * 
+     * @param modelType Type de testeur à utiliser
+     * @param config Propriétés de configuration
+     */
+    public SoundModelTester(SoundModelType modelType, Properties config) {
+        this.modelType = modelType;
+        this.config = config;
+        
+        // Créer le testeur approprié
+        createTester();
+    }
+    
+    /**
+     * Crée le testeur approprié en fonction du type spécifié
+     */
+    private void createTester() {
+        log.info("Création d'un testeur de son de type: {}", modelType);
+        
+        switch (modelType) {
+            case MFCC:
+                tester = new MFCCSoundModelTester(config);
+                break;
+                
+            case SPECTROGRAM:
+                tester = new SpectrogramSoundModelTester(config);
+                break;
+                
+            default:
+                log.warn("Type de testeur inconnu: {}. Utilisation de MFCC par défaut.", modelType);
+                tester = new MFCCSoundModelTester(config);
+                break;
+        }
     }
     
     @Override
-    protected double[] generateFeatureVectorForClass(int targetClass) {
-        double[] features = new double[inputSize];
-        
-        // Pour les sons, simuler différentes fréquences dominantes
-        for (int i = 0; i < inputSize; i++) {
-            // Amplitude de base pour cette classe
-            double baseAmplitude = 0.3 + (0.6 * targetClass / (numClasses - 1));
-            
-            // Fréquence "caractéristique" pour cette classe
-            double frequency = 1.0 + targetClass * 2.0;
-            
-            // Générer une forme d'onde avec cette fréquence
-            double wave = Math.sin(i * frequency * 2 * Math.PI / inputSize) * baseAmplitude;
-            
-            // Ajouter du bruit
-            double noise = random.nextGaussian() * 0.1;
-            
-            // Limiter les valeurs entre 0 et 1
-            features[i] = Math.max(0, Math.min(1, 0.5 + wave + noise));
-        }
-        
-        return features;
+    public boolean loadModel() {
+        return tester.loadModel();
     }
     
-/*    @Override
-    protected DataSetIterator createTestDataIterator() throws IOException {
-        // Utiliser la méthode de génération de données synthétiques de BaseModelTester
-        return new org.nd4j.linalg.dataset.api.iterator.TestDataSetIterator(generateTestData(), batchSize);
-    }*/
+    @Override
+    public EvaluationResult testModel() {
+        return tester.testModel();
+    }
+    
+    @Override
+    public boolean validateModel() {
+        return tester.validateModel();
+    }
+    
+    /**
+     * Retourne le type de testeur actuel
+     * 
+     * @return Type de testeur
+     */
+    public SoundModelType getModelType() {
+        return modelType;
+    }
+    
+    /**
+     * Change le type de testeur
+     * 
+     * @param modelType Nouveau type de testeur
+     */
+    public void setModelType(SoundModelType modelType) {
+        if (this.modelType != modelType) {
+            this.modelType = modelType;
+            createTester();
+        }
+    }
 }
