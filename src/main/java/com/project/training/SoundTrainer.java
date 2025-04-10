4c5509931a483456f8ce79616a9de408e5909efa
rbaudu <3866570+rbaudu@users.noreply.github.com>
2025-04-10 23:35:30 +0200

    Ajout de la classe de base SoundTrainer

--- src/main/java/com/project/training/SoundTrainer.java
+++ src/main/java/com/project/training/SoundTrainer.java
@@ -5,157 +5,107 @@ import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.IOException;
 import java.util.Properties;
+import java.util.Random;
 
 /**
- * Classe de façade pour les différents types d'entraîneurs de sons.
- * Cette classe délègue les tâches aux implémentations spécialisées.
+ * Classe abstraite de base pour l'entraînement des modèles de sons
  */
-public class SoundTrainer {
-    private static final Logger log = LoggerFactory.getLogger(SoundTrainer.class);
+public abstract class SoundTrainer {
+    private static final Logger logger = LoggerFactory.getLogger(SoundTrainer.class);
     
-    // Types d'entraîneurs de sons
-    public enum SoundTrainerType {
-        MFCC,
-        SPECTROGRAM_VGG16,
-        SPECTROGRAM_RESNET
-    }
-    
-    private BaseSoundTrainer trainer;
-    private SoundTrainerType trainerType;
-    private Properties config;
+    protected Properties config;
+    protected int numClasses;
+    protected int batchSize;
+    protected int numEpochs;
+    protected double learningRate;
+    protected int seed;
+    protected int hiddenLayerSize;
+    protected String modelType;
+    protected String dataDir;
+    protected String modelOutputPath;
     
     /**
      * Constructeur avec configuration
-     * 
-     * @param config Propriétés de configuration
      */
     public SoundTrainer(Properties config) {
         this.config = config;
         
-        // Déterminer le type d'entraîneur
-        String modelType = config.getProperty("sound.model.type", "STANDARD");
-        if ("SPECTROGRAM".equalsIgnoreCase(modelType)) {
-            String architecture = config.getProperty("sound.model.architecture", "VGG16");
-            if ("ResNet".equalsIgnoreCase(architecture)) {
-                this.trainerType = SoundTrainerType.SPECTROGRAM_RESNET;
-            } else {
-                this.trainerType = SoundTrainerType.SPECTROGRAM_VGG16;
-            }
+        // Charger les paramètres de base
+        this.numClasses = Integer.parseInt(config.getProperty("sound.model.num.classes", "2"));
+        this.batchSize = Integer.parseInt(config.getProperty("training.batch.size", "32"));
+        this.numEpochs = Integer.parseInt(config.getProperty("training.epochs", "100"));
+        this.learningRate = Double.parseDouble(config.getProperty("training.learning.rate", "0.0001"));
+        this.seed = Integer.parseInt(config.getProperty("training.seed", "42"));
+        this.hiddenLayerSize = Integer.parseInt(config.getProperty("model.hidden.size", "512"));
+        this.modelType = config.getProperty("sound.model.type", "STANDARD");
+        this.dataDir = config.getProperty("data.root.dir", "") + "/sound";
+        this.modelOutputPath = config.getProperty("models.root.dir", "") + "/sound";
+        
+        // Utiliser un générateur de nombres aléatoires fiable
+        Random rng = new Random(seed);
+    }
+    
+    /**
+     * Crée un entraîneur de sons selon le type spécifié
+     */
+    public static SoundTrainer createTrainer(String type, Properties config) {
+        logger.info("Création d'un entraîneur de sons de type: {}", type);
+        
+        if ("MFCC".equals(type)) {
+            return new MFCCSoundTrainer(config);
+        } else if ("SPECTROGRAM".equals(type)) {
+            return new SpectrogramSoundTrainer(config);
+        } else if ("SPECTROGRAM_VGG16".equals(type)) {
+            return new SpectrogramSoundTrainer(config, "VGG16");
         } else {
-            this.trainerType = SoundTrainerType.MFCC;
-        }
-        
-        // Créer l'entraîneur approprié
-        createTrainer();
-        
-        // Journaliser les paramètres d'entraînement
-        LoggingUtils.logSoundTrainingParameters(config);
-    }
-    
-    /**
-     * Constructeur avec type d'entraîneur spécifié
-     * 
-     * @param trainerType Type d'entraîneur à utiliser
-     * @param config Propriétés de configuration
-     */
-    public SoundTrainer(SoundTrainerType trainerType, Properties config) {
-        this.trainerType = trainerType;
-        this.config = config;
-        
-        // Créer l'entraîneur approprié
-        createTrainer();
-    }
-    
-    /**
-     * Crée l'entraîneur approprié en fonction du type spécifié
-     */
-    private void createTrainer() {
-        log.info("Création d'un entraîneur de sons de type: {}", trainerType);
-        
-        switch (trainerType) {
-            case MFCC:
-                trainer = new MFCCSoundTrainer(config);
-                break;
-                
-            case SPECTROGRAM_VGG16:
-                // Définir l'architecture VGG16
-                config.setProperty("sound.model.architecture", "VGG16");
-                trainer = new SpectrogramSoundTrainer(config);
-                break;
-                
-            case SPECTROGRAM_RESNET:
-                // Définir l'architecture ResNet
-                config.setProperty("sound.model.architecture", "ResNet");
-                trainer = new SpectrogramSoundTrainer(config);
-                break;
-                
-            default:
-                log.warn("Type d'entraîneur inconnu: {}. Utilisation de MFCC par défaut.", trainerType);
-                trainer = new MFCCSoundTrainer(config);
-                break;
+            logger.warn("Type d'entraîneur inconnu: {}, utilisation de MFCC par défaut", type);
+            return new MFCCSoundTrainer(config);
         }
     }
     
     /**
-     * Entraîne le modèle sur les données audio
-     * 
-     * @param dataDir Répertoire contenant les fichiers audio
-     * @param trainTestRatio Ratio pour la division train/test
-     * @throws IOException Si une erreur survient lors de la lecture des données ou de la sauvegarde du modèle
+     * Entraîne le modèle avec les données disponibles
      */
-    public void trainOnSoundData(String dataDir, double trainTestRatio) throws IOException {
-        log.info("Démarrage de l'entraînement de son avec l'entraîneur: {}", trainerType);
-        trainer.trainOnSoundData(dataDir, trainTestRatio);
-    }
-    
-    /**
-     * Initialise le modèle
-     */
-    public void initializeModel() {
-        trainer.initializeModel();
-    }
-    
-    /**
-     * Obtient le modèle entraîné
-     * 
-     * @return Le modèle entraîné
-     */
-    public MultiLayerNetwork getModel() {
-        return trainer.getModel();
-    }
-    
-    /**
-     * Retourne le type d'entraîneur actuel
-     * 
-     * @return Type d'entraîneur
-     */
-    public SoundTrainerType getTrainerType() {
-        return trainerType;
-    }
-    
-    /**
-     * Change le type d'entraîneur
-     * 
-     * @param trainerType Nouveau type d'entraîneur
-     */
-    public void setTrainerType(SoundTrainerType trainerType) {
-        if (this.trainerType != trainerType) {
-            this.trainerType = trainerType;
-            createTrainer();
-        }
-    }
-    
-    /**
-     * Entraîne le modèle avec la configuration par défaut
-     * 
-     * @throws IOException Si une erreur survient lors de l'entraînement
-     */
-    public void train() throws IOException {
-        String dataDir = config.getProperty("sound.data.dir", "data/raw/sound");
-        double trainTestRatio = Double.parseDouble(config.getProperty("training.train.ratio", "0.8"));
+    public MultiLayerNetwork train() {
+        // Prétraiter les données
+        preprocessData();
         
-        trainOnSoundData(dataDir, trainTestRatio);
+        // Créer le modèle
+        MultiLayerNetwork model = createModel();
+        
+        // Afficher les informations sur le modèle et les paramètres d'entraînement
+        logTrainingInfo();
+        
+        // TODO: Implémenter l'entraînement réel du modèle
+        
+        return model;
     }
+    
+    /**
+     * Affiche les informations d'entraînement
+     */
+    protected void logTrainingInfo() {
+        LoggingUtils.logSeparator();
+        logger.info("=== Paramètres d'entraînement du modèle de son ===");
+        logger.info("Type de modèle: {}", modelType);
+        logger.info("Nombre de classes: {}", numClasses);
+        logger.info("Taille du lot: {}", batchSize);
+        logger.info("Nombre d'époques: {}", numEpochs);
+        logger.info("Taux d'apprentissage: {}", learningRate);
+        logger.info("Répertoire de données: {}", dataDir);
+        logger.info("Chemin du modèle: {}", modelOutputPath);
+        logger.info("=== Fin des paramètres d'entraînement ===");
+        LoggingUtils.logSeparator();
+    }
+    
+    /**
+     * Crée le modèle à entraîner
+     */
+    protected abstract MultiLayerNetwork createModel();
+    
+    /**
+     * Prétraite les données d'entraînement
+     */
+    protected abstract void preprocessData();
 }
